24e86f0725a8f0dceea93bfacb61b4a815b83dc2
G_alumi <trainboy1024k@gmail.com>
2025-08-13 00:55:00 +0900

    5300_newの画像をトリミング

--- autopak.bat
+++ autopak.bat
@@ -10,4 +10,4 @@ cd /d %~dp0
 makeobj list *.pak > addonList.txt
 copy *.pak D:\Users\G_alumi\Simutrans\game\Simutrans_OTRP_test\pak128.japan_120
 taskkill /t /im testrans.exe
-start D:\Users\G_alumi\Simutrans\game\Simutrans_OTRP_test\testrans.exe -objects pak128.japan_120/ -pause
+start D:\Users\G_alumi\Simutrans\game\Simutrans_OTRP_test\testrans.exe -objects pak128.japan_120/
