19012e67ffcf65826c001b0db8b3547bd071d808
G_alumi <trainboy1024k@gmail.com>
2025-08-14 23:02:21 +0900

    ディレクトリ名変更

--- autopak.bat
+++ autopak.bat
@@ -2,7 +2,7 @@
 cd /d %~dp0/vehicle
 
 for /d %%d in ("./*") do (
-	makeobj pak128 ../vehicle.Gal_hankyu%%d.pak ./%%d/*.dat
+	makeobj pak128 ../vehicle.%%d.pak ./%%d/*.dat
 	@REM echo %%d 終了コード:%errorlevel%
 )
 
